54a040c736ff5bae001130654a9ac9901ed428a4
Roshan <chyroshan066@gmail.com>
2025-08-16 00:36:06 +0545

    feat(ui): add responsive header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,98 +1,200 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { IonIcon } from "./utility/IonIcon";
+
 export const Header = () => {
+    const [isNavActive, setIsNavActive] = useState(false);
+    const [isHeaderActive, setIsHeaderActive] = useState(false);
+
+    const toggleNav = () => {
+        setIsNavActive(prev => !prev);
+    };
+
+    const closeNav = () => {
+        setIsNavActive(false);
+    };
+
+    useEffect(() => {
+        const handleScroll = () => {
+            if (window.scrollY >= 100) {
+                setIsHeaderActive(true);
+            } else {
+                setIsHeaderActive(false);
+            }
+        };
+
+        window.addEventListener("scroll", handleScroll);
+
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        };
+    }, []);
+
     return <>
-        <header class="header">
+        <header className={`header ${isHeaderActive ? 'active' : ''}`}>
+            <div className="header-top">
+                <div className="custom-container">
+                    <ul className="contact-list">
 
-            <div class="header-top">
-                <div class="container">
-
-                    <ul class="contact-list">
-
-                        <li class="contact-item">
-                            <ion-icon name="mail-outline"></ion-icon>
-
-                            <a href="mailto:info@example.com" class="contact-link">info@example.com</a>
-                        </li>
-
-                        <li class="contact-item">
-                            <ion-icon name="call-outline"></ion-icon>
-
-                            <a href="tel:+917052101786" class="contact-link">+91-7052-101-786</a>
-                        </li>
-
-                    </ul>
-
-                    <ul class="social-list">
-
-                        <li>
-                            <a href="#" class="social-link">
-                                <ion-icon name="logo-facebook"></ion-icon>
+                        <li className="contact-item">
+                            <IonIcon name="mail-outline" />
+                            <a
+                                href="mailto:info@example.com"
+                                className="contact-link"
+                            >
+                                info@example.com
                             </a>
                         </li>
 
-                        <li>
-                            <a href="#" class="social-link">
-                                <ion-icon name="logo-instagram"></ion-icon>
-                            </a>
-                        </li>
-
-                        <li>
-                            <a href="#" class="social-link">
-                                <ion-icon name="logo-twitter"></ion-icon>
-                            </a>
-                        </li>
-
-                        <li>
-                            <a href="#" class="social-link">
-                                <ion-icon name="logo-youtube"></ion-icon>
+                        <li className="contact-item">
+                            <IonIcon name="call-outline" />
+                            <a
+                                href="tel:+917052101786"
+                                className="contact-link"
+                            >
+                                +91-7052-101-786
                             </a>
                         </li>
 
                     </ul>
+                    <ul className="social-list">
 
+                        <li>
+                            <a
+                                href="#"
+                                className="social-link"
+                            >
+                                <IonIcon name="logo-facebook" />
+                            </a>
+                        </li>
+
+                        <li>
+                            <a
+                                href="#"
+                                className="social-link"
+                            >
+                                <IonIcon name="logo-instagram" />
+                            </a>
+                        </li>
+
+                        <li>
+                            <a
+                                href="#"
+                                className="social-link"
+                            >
+                                <IonIcon name="logo-twitter" />
+                            </a>
+                        </li>
+
+                        <li>
+                            <a
+                                href="#"
+                                className="social-link"
+                            >
+                                <IonIcon name="logo-youtube" />
+                            </a>
+                        </li>
+
+                    </ul>
                 </div>
             </div>
-
-            <div class="header-bottom" data-header>
-                <div class="container">
-
-                    <a href="#" class="logo">Dentelo.</a>
-
-                    <nav class="navbar container" data-navbar>
-                        <ul class="navbar-list">
-
+            <div
+                className="header-bottom"
+                data-header
+            >
+                <div className="custom-container">
+                    <a
+                        href="#"
+                        className="logo"
+                    >
+                        Dentelo.
+                    </a>
+                    <nav
+                        className={`navbar custom-container ${isNavActive ? 'active' : ''}`}
+                        data-navbar
+                    >
+                        <ul className="navbar-list">
                             <li>
-                                <a href="#home" class="navbar-link" data-nav-link>Home</a>
+                                <a
+                                    href="#home"
+                                    className="navbar-link"
+                                    onClick={closeNav}
+                                    data-nav-link
+                                >
+                                    Home
+                                </a>
                             </li>
-
                             <li>
-                                <a href="#service" class="navbar-link" data-nav-link>Services</a>
+                                <a
+                                    href="#service"
+                                    className="navbar-link"
+                                    onClick={closeNav}
+                                    data-nav-link
+                                >
+                                    Services
+                                </a>
                             </li>
-
                             <li>
-                                <a href="#about" class="navbar-link" data-nav-link>About Us</a>
+                                <a
+                                    href="#about"
+                                    className="navbar-link"
+                                    onClick={closeNav}
+                                    data-nav-link
+                                >
+                                    About Us
+                                </a>
                             </li>
-
                             <li>
-                                <a href="#blog" class="navbar-link" data-nav-link>Blog</a>
+                                <a
+                                    href="#blog"
+                                    className="navbar-link"
+                                    onClick={closeNav}
+                                    data-nav-link
+                                >
+                                    Blog
+                                </a>
                             </li>
-
                             <li>
-                                <a href="#" class="navbar-link" data-nav-link>Contact</a>
+                                <a
+                                    href="#"
+                                    className="navbar-link"
+                                    onClick={closeNav}
+                                    data-nav-link
+                                >
+                                    Contact
+                                </a>
                             </li>
-
                         </ul>
                     </nav>
 
-                    <a href="#" class="btn">Book appointment</a>
+                    <a
+                        href="#"
+                        className="btn"
+                    >
+                        Book appointment
+                    </a>
 
-                    <button class="nav-toggle-btn" aria-label="Toggle menu" data-nav-toggler>
-                        <ion-icon name="menu-sharp" aria-hidden="true" class="menu-icon"></ion-icon>
-                        <ion-icon name="close-sharp" aria-hidden="true" class="close-icon"></ion-icon>
+                    <button
+                        className={`nav-toggle-btn ${isNavActive ? 'active' : ''}`}
+                        aria-label="Toggle menu"
+                        onClick={toggleNav}
+                        data-nav-toggler
+                    >
+                        <IonIcon
+                            name="menu-sharp"
+                            aria-hidden="true"
+                            className="menu-icon"
+                        />
+                        <IonIcon
+                            name="close-sharp"
+                            aria-hidden="true"
+                            className="close-icon"
+                        />
                     </button>
 
                 </div>
             </div>
-
         </header>
     </>;
 }
